d3b9e48484151875f34b88dcfacc622fc62fbc7f
Gundam <abc@abc.com>
2025-06-21 11:41:42 -0700

    Add debug info

--- src/test/java/com/gundam/app/ReadTest.java
+++ src/test/java/com/gundam/app/ReadTest.java
@@ -76,17 +76,12 @@ public class ReadTest {
 		page.getByTestId("fr-field-callback_1").getByTestId("input-").press("Tab");
 		page.getByTestId("fr-field-callback_2").getByTestId("input-").fill("Cop@9075");
 		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign In")).click();
-		// page.locator("label").filter(new Locator.FilterOptions().setHasText("Not
-		// Now")).click();
-		// page.getByRole(AriaRole.BUTTON, new
-		// Page.GetByRoleOptions().setName("Continue")).click();
 
 		page.waitForTimeout(5000);
 
 		Locator landingTitle = page.getByText("Business Customer Onboarding");
 		assertThat(landingTitle).isVisible();
-		assertTrue(landingTitle.textContent().contains("Business Customer Onboarding"),
-				"Incorrect text for My Business Locations");
+		assertTrue(landingTitle.textContent().contains("Business Customer Onboarding"), "Incorrect text for My Business Locations");
 
 		// Assertion with exact string match
 		assertThat(page.locator("div:has-text('successfully registered your USPS Business Account.')").nth(3))
@@ -95,89 +90,91 @@ public class ReadTest {
 		Locator yourCompanyInfo = page.getByText("Your Company Information");
 		assertThat(yourCompanyInfo).isVisible();
 		assertTrue(yourCompanyInfo.textContent().contains("Your Company Information"));
+		assertTrue(yourCompanyInfo.textContent().contains("Your Company Information"), "Incorrect text for Your Company Information");
 
 		Locator addressCatMerchant = page.getByText("Address CAT Merchant Test Inc");
 		assertThat(addressCatMerchant).isVisible();
 		System.out.println("Actual text content: " + addressCatMerchant.textContent());
-		assertTrue(addressCatMerchant.textContent().contains("CAT Merchant Test"));
+		assertTrue(addressCatMerchant.textContent().contains("CAT Merchant Test"), "Incorrect CAT Merchant Test");
 
 		Locator nameCatMerchantPhone = page.getByText("Name CAT MerchantPhone Number");
 		assertThat(nameCatMerchantPhone).isVisible();
-		assertTrue(nameCatMerchantPhone.textContent().contains("Name CAT MerchantPhone Number"));
+		assertTrue(nameCatMerchantPhone.textContent().contains("Name CAT MerchantPhone Number"), "Incorrect CAT MerchantPhone Number");
 
 		Locator email = page.getByText("Email");
 		assertThat(email).isVisible();
-		assertTrue(email.textContent().contains("Email"));
+		assertTrue(email.textContent().contains("Email"), "Incorrect Email Label");
 
 		Locator businessAccountHas = page.getByText("Your business account has");
 		assertThat(businessAccountHas).isVisible();
-		assertTrue(businessAccountHas.textContent().contains("Your business account has"));
+		assertTrue(businessAccountHas.textContent().contains("Your business account has"), "Incorrect Your business account Label");
 
 		Locator cridLabel = page.getByText("Customer Registration ID (CRID):");
 		assertThat(cridLabel).isVisible();
-		assertTrue(cridLabel.textContent().contains("Customer Registration ID (CRID):"));
+		assertTrue(cridLabel.textContent().contains("Customer Registration ID (CRID):"), "Incorrect Customer Registration ID Label");
 
 		Locator outboundMidLabel = page.getByText("Outbound Mailer ID (MID):");
 		assertThat(outboundMidLabel).isVisible();
-		assertTrue(outboundMidLabel.textContent().contains("Outbound Mailer ID (MID):"));
+		assertTrue(outboundMidLabel.textContent().contains("Outbound Mailer ID (MID):"), "Incorrect Outbound Mailer ID Label");
 
 		Locator returnMidLabel = page.getByText("Return Mailer ID (MID):");
 		assertThat(returnMidLabel).isVisible();
-		assertTrue(returnMidLabel.textContent().contains("Return Mailer ID (MID):"));
+		assertTrue(returnMidLabel.textContent().contains("Return Mailer ID (MID):"), "Incorrect Return Mailer ID");
 
 		Locator returnMidValue = page.getByText("Return Mailer ID (MID): 901117831");
 		assertThat(returnMidValue).isVisible();
-		assertTrue(returnMidValue.textContent().contains("Return Mailer ID (MID): 901117831"));
+		assertTrue(returnMidValue.textContent().contains("901117831"), "Incorrect 901117831");
 
 		Locator outboundMidValue = page.getByText("Outbound Mailer ID (MID): 901117829 (MASTER)");
 		assertThat(outboundMidValue).isVisible();
-		assertTrue(outboundMidValue.textContent().contains("Outbound Mailer ID (MID): 901117829 (MASTER)"));
+		assertTrue(outboundMidValue.textContent().contains("Outbound Mailer ID (MID): 901117829 (MASTER)"), "Incorrect Outbound Mailer ID");
 
 		Locator cridValue = page.getByText("Customer Registration ID (CRID): 95178918");
 		assertThat(cridValue).isVisible();
-		assertTrue(cridValue.textContent().contains("Customer Registration ID (CRID): 95178918"));
+		assertTrue(cridValue.textContent().contains("Customer Registration ID (CRID): 95178918"), "Incorrect Customer Registration ID");
 
 		Locator businessLocationsFaqs = page.getByText("Business Locations FAQs");
 		assertThat(businessLocationsFaqs).isVisible();
-		assertTrue(businessLocationsFaqs.textContent().contains("Business Locations FAQs"));
+		assertTrue(businessLocationsFaqs.textContent().contains("Business Locations FAQs"), "Incorrect Business Locations FAQs");
 
 		Locator whatIsCrid = page.getByText("What is a CRID?");
 		assertThat(whatIsCrid).isVisible();
-		assertTrue(whatIsCrid.textContent().contains("What is a CRID?"));
+		assertTrue(whatIsCrid.textContent().contains("What is a CRID?"), "Incorrect What is a CRID");
 
 		Locator cridDescription = page
 				.getByText("The Customer Registration ID (CRID) is a number that identifies your physical");
 		assertThat(cridDescription).isVisible();
 		assertTrue(cridDescription.textContent()
-				.contains("The Customer Registration ID (CRID) is a number that identifies your physical"));
+				.contains("The Customer Registration ID (CRID) is a number that identifies your physical"), "Incorrect The Customer Registration ID");
 
 		Locator whatIsMid = page.getByText("What is a MID?");
 		assertThat(whatIsMid).isVisible();
-		assertTrue(whatIsMid.textContent().contains("What is a MID?"));
+		assertTrue(whatIsMid.textContent().contains("What is a MID?"), "Incorrect What is a MID");
 
-		Locator midDescription = page.getByText("The Mailer Identification (");
+		Locator midDescription = page.getByText("The Mailer Identification");
 		assertThat(midDescription).isVisible();
-		assertTrue(midDescription.textContent().contains("The Mailer Identification ("));
+		assertTrue(midDescription.textContent().contains("The Mailer Identification"), "Incorrect The Mailer Identification");
 
 		Locator whatIsEps = page.getByText("What is an EPS Account Number?");
 		assertThat(whatIsEps).isVisible();
-		assertTrue(whatIsEps.textContent().contains("What is an EPS Account Number?"));
+		assertTrue(whatIsEps.textContent().contains("What is an EPS Account Number?"), "Incorrect What is an EPS Account Number");
 
 		Locator epsDescription = page.getByText("The Enterprise Payment System");
 		assertThat(epsDescription).isVisible();
-		assertTrue(epsDescription.textContent().contains("The Enterprise Payment System"));
+		assertTrue(epsDescription.textContent().contains("The Enterprise Payment System"), "Incorrect The Enterprise Payment System");
 
 		Locator youCanAdd = page.getByText("You can add additional");
 		assertThat(youCanAdd).isVisible();
-		assertTrue(youCanAdd.textContent().contains("You can add additional"));
+		assertTrue(youCanAdd.textContent().contains("You can add additional"), "Incorrect You can add additional");
 
 		Locator epsAccountInfo = page.getByText("Enterprise Payment System (EPS) Account Information");
 		assertThat(epsAccountInfo).isVisible();
-		assertTrue(epsAccountInfo.textContent().contains("Enterprise Payment System (EPS) Account Information"));
+		assertTrue(epsAccountInfo.textContent().contains("Enterprise Payment System (EPS) Account Information"), "Incorrect Enterprise Payment System (EPS) Account Information");
 
 		Locator chooseExistingActive = page.getByText("Choose an existing, active");
 		assertThat(chooseExistingActive).isVisible();
-		assertTrue(chooseExistingActive.textContent().contains("Choose an existing, active"));
+		assertTrue(chooseExistingActive.textContent().contains("Choose an existing, active"), "Incorrect Choose an existing active");
+
 		page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Logout")).click();
 		page.navigate("https://cop-cat.usps.com/");
 	}
